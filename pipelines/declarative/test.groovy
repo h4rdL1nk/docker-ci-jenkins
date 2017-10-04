525cb2892c147af3c3fcea176e79227735b1d9c5
luismiguel.saez <luismiguel.saez@madisonmk.com>
2017-10-04 12:31:13 +0200

    Added node properties

--- pipelines/declarative/test.groovy
+++ pipelines/declarative/test.groovy
@@ -1,6 +1,19 @@
 pipeline {
-    agent {
-        label 'worker'
+    node{
+        properties([
+            pipelineTriggers([
+                [$class: 'GenericTrigger',
+                genericVariables: [
+                 [expressionType: 'JSONPath', key: 'reference', value: '$.actor.username']
+                ],
+                regexpFilterText: '',
+                regexpFilterExpression: ''
+                ]
+            ])
+        ])
+        agent {
+            label 'worker'
+        }
     }
     options {
         timestamps()
@@ -14,15 +27,17 @@ pipeline {
         }
     }
     post {
-                failure {
-                        emailext(
-                                from: "jenkins-ci@app.madisonmk.com",
-                                to: "luismiguel.saez@madisonmk.com",
-                                mimeType: 'text/html',
-                                subject: "[${currentBuild.currentResult}] ${BUILD_DISPLAY_NAME} ${JOB_NAME}",
-                                body: "<br>Finalizado ${JOB_NAME} ${BUILD_NUMBER}<br>Nodo:${NODE_NAME}",
-                                attachLog: true
-                                )
-                }
+        failure {
+            emailext(
+                from: "jenkins-ci@app.madisonmk.com",
+                to: "luismiguel.saez@madisonmk.com",
+                mimeType: 'text/html',
+                subject: "[${currentBuild.currentResult}] ${BUILD_DISPLAY_NAME} ${JOB_NAME}",
+                body: "<br>Finalizado ${JOB_NAME} ${BUILD_NUMBER}<br>Nodo:${NODE_NAME}",
+                attachLog: true
+            )
+        }
     }
 }
+
+
