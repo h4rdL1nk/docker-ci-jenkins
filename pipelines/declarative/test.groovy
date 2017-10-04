7175538c713539492f2c9bf8a3e5c71bebb6d941
luismiguel.saez <luismiguel.saez@madisonmk.com>
2017-10-04 07:40:37 +0200

    Added test pipelines

---
+++ pipelines/declarative/test.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent {
+                label 'worker'
+    }
+    parameters {
+    }
+    options {
+        timestamps()
+        disableConcurrentBuilds()
+    }
+    stages {
+        stage('Show data'){
+            steps{
+                echo "Printing data ..."
+            }
+        }
+        post {
+                failure {
+                        emailext(
+                                from: "jenkins-ci@app.madisonmk.com",
+                                to: "luismiguel.saez@madisonmk.com",
+                                mimeType: 'text/html',
+                                subject: "[${currentBuild.currentResult}] ${BUILD_DISPLAY_NAME} ${JOB_NAME}",
+                                body: "<br>Finalizado ${JOB_NAME} ${BUILD_NUMBER}<br>Nodo:${NODE_NAME}",
+                                attachLog: true
+                                )
+                }
+        }
+    }
+}
