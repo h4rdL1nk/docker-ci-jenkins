a51e72ab9e13e572f009cfeb4f2760fd059b939b
luismiguel.saez <luismiguel.saez@madisonmk.com>
2017-10-05 07:56:14 +0200

    Finished pipeline with Bitbucket info ( json )

--- pipelines/declarative/README.md
+++ pipelines/declarative/README.md
@@ -1,22 +1,25 @@
 
+Webhook
+	https://user:pass@jenkins.host/generic-webhook-trigger/invoke
 
+Jenkins job definition
 
-Buil Triggers
-	Generic Webhook Trigger
-		Post content parameters
-			Variable: GIT_REPOSITORY
-			Expression: $.repository.name
-			Type: JSONPath
+	Buil Triggers
+		Generic Webhook Trigger
+			Post content parameters
+				Variable: GIT_REPOSITORY
+				Expression: $.repository.name
+				Type: JSONPath
 
-			Variable: GIT_USERNAME
-			Expression: $.actor.username
-			Type: JSONPath
+				Variable: GIT_USERNAME
+				Expression: $.repository.owner.username
+				Type: JSONPath
 
-			Variable: GIT_BRANCH
-			Expression: $.push.changes.new.name
-			Type: JSONPath
+				Variable: GIT_BRANCH
+				Expression: $.push.changes
+				Type: JSONPath
 
-Pipeline
-	Pipeline Script from SCM
-		docker-build-test-push.groovy
+	Pipeline
+		Pipeline Script from SCM
+			docker-build-test-push.groovy
 
--- pipelines/declarative/docker-build-test-push.groovy
+++ pipelines/declarative/docker-build-test-push.groovy
@@ -8,7 +8,6 @@ pipeline {
         label 'workeraws'
     }
     parameters {
-        string(name: 'GIT_BRANCH' , defaultValue: 'master')
         string(name: 'DEPARTMENT')
         string(name: 'AWS_REGION', defaultValue: 'eu-west-1')
         string(name: 'APP_NAME')
@@ -21,21 +20,23 @@ pipeline {
     stages {
         stage('Checkout code'){
             steps{
-                                script{
-                                        codeCo = checkout scm:
-                                                [
-                                                        $class: 'GitSCM',
-                                                        poll: true,
-                                                        extensions: [[
-                                                                $class: 'RelativeTargetDirectory',
-                                                                relativeTargetDir: 'code'
-                                                        ]],
-                                                        branches: [[
-                                                                name: '*/master'
-                                                        ]],
-                                                        userRemoteConfigs: [[credentialsId: '1bab7e77-96a9-4fba-9b6d-d0d49b93345c',url: 'git@bitbucket.org:${GIT_USERNAME}/${GIT_REPOSITORY}']]
-                                                ]
-                                }
+                script{
+                    codeCo = checkout scm:[
+                                $class: 'GitSCM',
+                                poll: true,
+                                extensions: [[
+                                    $class: 'RelativeTargetDirectory',
+                                    relativeTargetDir: 'code'
+                                ]],
+                                branches: [[
+                                    name: '*/${GIT_BRANCH_0_new_name}'
+                                ]],
+                                userRemoteConfigs: [[
+                                    credentialsId: '1bab7e77-96a9-4fba-9b6d-d0d49b93345c',
+                                    url: 'git@bitbucket.org:${GIT_USERNAME}/${GIT_REPOSITORY}'
+                                ]]
+                            ]
+                }
             }
         }
         stage('Docker image build') {
@@ -51,10 +52,10 @@ pipeline {
         stage('Application acceptance tests') {
             steps{
                 sh script: """
-                                docker run -d -e ENV=dev --name jenkins-${JOB_NAME}-${BUILD_NUMBER}-run jenkins-${JOB_NAME}-${BUILD_NUMBER}-img
-                                docker exec -i jenkins-${JOB_NAME}-${BUILD_NUMBER}-run composer require 'codeception/codeception:*'
-                                docker exec -i jenkins-${JOB_NAME}-${BUILD_NUMBER}-run php vendor/bin/codecept run --no-colors --json
-                        """
+                    docker run -d -e ENV=dev --name jenkins-${JOB_NAME}-${BUILD_NUMBER}-run jenkins-${JOB_NAME}-${BUILD_NUMBER}-img
+                    docker exec -i jenkins-${JOB_NAME}-${BUILD_NUMBER}-run composer require 'codeception/codeception:*'
+                    docker exec -i jenkins-${JOB_NAME}-${BUILD_NUMBER}-run php vendor/bin/codecept run --no-colors --json
+                    """
             }
         }
         stage('Push image to AWS') {
@@ -71,11 +72,10 @@ pipeline {
                         echo "Tag AWS: ${imgAwsTag}"
 
                         sh script: """
-                                                        docker login -u AWS -p ${login_token} ${login_endpt}
-                                                        docker tag jenkins-${JOB_NAME}-${BUILD_NUMBER}-img ${imgAwsTag}
-                                                        docker push ${imgAwsTag}
-                                                        """, returnStdout: true
-
+                            docker login -u AWS -p ${login_token} ${login_endpt}
+                            docker tag jenkins-${JOB_NAME}-${BUILD_NUMBER}-img ${imgAwsTag}
+                            docker push ${imgAwsTag}
+                            """, returnStdout: true
                     }
                 }
             }
@@ -87,81 +87,80 @@ pipeline {
                         imgTag = codeCo.GIT_COMMIT
                         imgLocalTag = "registry.madisonmk.com/${DEPARTMENT}/${APP_NAME}:${imgTag}"
                         sh script: """
-                                        docker tag jenkins-${JOB_NAME}-${BUILD_NUMBER}-img ${imgLocalTag}
-                                        docker push ${imgLocalTag}
-                                        """, returnStdout: true
+                            docker tag jenkins-${JOB_NAME}-${BUILD_NUMBER}-img ${imgLocalTag}
+                            docker push ${imgLocalTag}
+                            """, returnStdout: true
                     }
                 }
             }
         }
         stage('Deploy application'){
-                when{
-                        expression {
-                                return DEPLOY
-                        }
+            when{
+                expression {
+                    return DEPLOY
                 }
-                steps{
-                        withAWS(region:"${AWS_REGION}",credentials:"aws-${DEPARTMENT}-admin"){
-                                script{
-                                        awsEnv = "pro"
+            }
+            steps{
+                withAWS(region:"${AWS_REGION}",credentials:"aws-${DEPARTMENT}-admin"){
+                    script{
+                        awsEnv = "pro"
 
-                                        echo "Desplegando imagen: ${imgAwsTag}"
+                        echo "Desplegando imagen: ${imgAwsTag}"
 
-                                        def AwsCluster = sh script: """
-                                                        aws ecs list-clusters | jq -r '.clusterArns[]|select(test("^.*CL.*-${awsEnv}\$"))'
-                                                """, returnStdout: true
-                                        def AwsService = sh script: """
-                                                        aws ecs list-services --cluster ${AwsCluster.trim()} | jq -r '.serviceArns[]|select(test("^.*:service/SVC-${APP_NAME}(-${awsEnv})?.*\$"))'
-                                                """, returnStdout: true
-                                        def AwsTaskDef = sh script: """
-                                                        aws ecs describe-services --cluster ${AwsCluster.trim()} --services ${AwsService.trim()} | jq -r '.services[].deployments[].taskDefinition'
-                                                """, returnStdout: true
-                                        def AwsTaskDefJson = sh script: """
-                                                        aws ecs describe-task-definition --task-definition ${AwsTaskDef.trim()} | jq -rc '.taskDefinition|.containerDefinitions[].image="'${imgAwsTag}'"|if .volumes != null then .volumes=.volumes else .volumes=[] end|if .networkMode != null then .networkMode=.networkMode else .networkMode="bridge" end|if .placementConstraints != null then .placementConstraints=.placementConstraints else .placementConstraints=[] end|{family:.family,containerDefinitions:.containerDefinitions,volumes:.volumes,placementConstraints:.placementConstraints,networkMode:.networkMode}'
-                                                """, returnStdout: true
-                                        def AwsTaskDefArn = sh script: """
-                                                        aws ecs register-task-definition --cli-input-json '${AwsTaskDefJson.trim()}' | jq -r '.taskDefinition.taskDefinitionArn'
-                                                """, returnStdout: true
+                        def AwsCluster = sh script: """
+                            aws ecs list-clusters | jq -r '.clusterArns[]|select(test("^.*CL.*-${awsEnv}\$"))'
+                            """, returnStdout: true
+                        def AwsService = sh script: """
+                            aws ecs list-services --cluster ${AwsCluster.trim()} | jq -r '.serviceArns[]|select(test("^.*:service/SVC-${APP_NAME}(-${awsEnv})?.*\$"))'
+                            """, returnStdout: true
+                        def AwsTaskDef = sh script: """
+                            aws ecs describe-services --cluster ${AwsCluster.trim()} --services ${AwsService.trim()} | jq -r '.services[].deployments[].taskDefinition'
+                            """, returnStdout: true
+                        def AwsTaskDefJson = sh script: """
+                            aws ecs describe-task-definition --task-definition ${AwsTaskDef.trim()} | jq -rc '.taskDefinition|.containerDefinitions[].image="'${imgAwsTag}'"|if .volumes != null then .volumes=.volumes else .volumes=[] end|if .networkMode != null then .networkMode=.networkMode else .networkMode="bridge" end|if .placementConstraints != null then .placementConstraints=.placementConstraints else .placementConstraints=[] end|{family:.family,containerDefinitions:.containerDefinitions,volumes:.volumes,placementConstraints:.placementConstraints,networkMode:.networkMode}'
+                            """, returnStdout: true
+                        def AwsTaskDefArn = sh script: """
+                            aws ecs register-task-definition --cli-input-json '${AwsTaskDefJson.trim()}' | jq -r '.taskDefinition.taskDefinitionArn'
+                            """, returnStdout: true
+                        def AwsSvcUpdatedTask = sh script: """
+                            aws ecs update-service --cluster ${AwsCluster.trim()} --service ${AwsService.trim()} --task-definition ${AwsTaskDefArn.trim()} | jq -r '.service.taskDefinition'
+                            """, returnStdout: true
 
-                                        def AwsSvcUpdatedTask = sh script: """
-                                                        aws ecs update-service --cluster ${AwsCluster.trim()} --service ${AwsService.trim()} --task-definition ${AwsTaskDefArn.trim()} | jq -r '.service.taskDefinition'
-                                                """, returnStdout: true
+                        echo "Service updated with ${AwsSvcUpdatedTask.trim()}"
 
-                                        echo "Service updated with ${AwsSvcUpdatedTask.trim()}"
-
-                                        echo "Finished build ${JOB_NAME}:${BUILD_NUMBER} at node ${NODE_NAME}"
-                                }
-                        }
+                        echo "Finished build ${JOB_NAME}:${BUILD_NUMBER} at node ${NODE_NAME}"
+                    }
                 }
+            }
         }
-        }
-        post {
-                always {
-                        emailext(
-                                from: "jenkins-ci@app.madisonmk.com",
-                                to: "luismiguel.saez@madisonmk.com",
-                                mimeType: 'text/html',
-                                subject: "[${currentBuild.currentResult}] ${BUILD_DISPLAY_NAME} ${JOB_NAME}",
-                                body: "<br>Finalizado ${JOB_NAME} ${BUILD_NUMBER}<br>Nodo:${NODE_NAME}",
-                                attachLog: true
-                                )
+    }
+    post {
+        always {
+            emailext(
+                from: "jenkins-ci@app.madisonmk.com",
+                to: "luismiguel.saez@madisonmk.com",
+                mimeType: 'text/html',
+                subject: "[${currentBuild.currentResult}] ${BUILD_DISPLAY_NAME} ${JOB_NAME}",
+                body: "<br>Finalizado ${JOB_NAME} ${BUILD_NUMBER}<br>Nodo:${NODE_NAME}",
+                attachLog: true
+            )
 
-                        deleteDir()
+            deleteDir()
 
-                        sh script: """
-                                if `docker inspect jenkins-${JOB_NAME}-${BUILD_NUMBER}-run >/dev/null 2>&1`
-                                then
-                                        docker rm --force jenkins-${JOB_NAME}-${BUILD_NUMBER}-run
-                                fi
-                                if `docker inspect jenkins-${JOB_NAME}-${BUILD_NUMBER}-img >/dev/null 2>&1`
-                                then
-                                        docker rmi jenkins-${JOB_NAME}-${BUILD_NUMBER}-img
-                                fi
-                                if `docker inspect ${imgAwsTag} >/dev/null 2>&1`
-                                then
-                                        docker rmi ${imgAwsTag}
-                                fi
-                        """, returnStdout: false
-                }
+            sh script: """
+                if `docker inspect jenkins-${JOB_NAME}-${BUILD_NUMBER}-run >/dev/null 2>&1`
+                then
+                    docker rm --force jenkins-${JOB_NAME}-${BUILD_NUMBER}-run
+                fi
+                if `docker inspect jenkins-${JOB_NAME}-${BUILD_NUMBER}-img >/dev/null 2>&1`
+                then
+                    docker rmi jenkins-${JOB_NAME}-${BUILD_NUMBER}-img
+                fi
+                if `docker inspect ${imgAwsTag} >/dev/null 2>&1`
+                then
+                    docker rmi ${imgAwsTag}
+                fi
+                """, returnStdout: false
         }
+    }
 }
