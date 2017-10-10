1353784fbec0c3c1175694defbfb52a32d708675
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 12:07:39 +0200

    Added function call for AWS image push

--- pipelines/declarative/docker-build-test-push.groovy
+++ pipelines/declarative/docker-build-test-push.groovy
@@ -90,27 +90,16 @@ pipeline {
         }
         stage('Push image to AWS') {
             steps{
-                withAWS(region:"${awsRegion}",credentials:"aws-${DEPARTMENT}-admin"){
-                    script {
-                        def create_repo_cmd = sh script: "aws ecr create-repository --repository-name ${APP_NAME}"
-                        def login_cmd = sh script: "aws ecr get-login --no-include-email", returnStdout: true
-                        //def login_token = login_cmd.split(' ')[5].trim()
-                        //def login_endpt = login_cmd.split(' ')[8].trim()
-
-                        imgTag = codeCo.GIT_COMMIT
-                        imgAwsTag = "${login_endpt.split('//')[1]}/${APP_NAME}:${imgTag}"
-
-                        echo "Tag AWS: ${imgAwsTag}"
-
-                        sh script: """
-                            #docker login -u AWS -p ${login_token} ${login_endpt}
-                            ${login_cmd}
-                            docker tag jenkins-${JOB_NAME}-${BUILD_NUMBER}-img ${imgAwsTag}
-                            docker push ${imgAwsTag}
-                            """, returnStdout: true
-                    }
+                script{
+                    dockerPushImageAws([
+                        awsRegion: "${awsRegion}",
+                        awsCredId: "aws-${DEPARTMENT}-admin" 
+                        localImageTag: "jenkins-${JOB_NAME}-${BUILD_NUMBER}-img",
+                        pushImageTag: "${APP_NAME}:${codeCo.GIT_COMMIT}"
+                    ])   
                 }
             }
+        } 
         }
         stage('Deploy application'){
             when{
