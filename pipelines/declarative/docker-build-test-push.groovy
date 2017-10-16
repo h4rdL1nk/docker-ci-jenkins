0327e8713cef93b5f589511de413d901e4a859ad
h4rdL1nk <imthedanger@protonmail.com>
2017-10-16 08:29:35 +0200

    Added parameters

--- pipelines/declarative/docker-build-test-push.groovy
+++ pipelines/declarative/docker-build-test-push.groovy
@@ -105,6 +105,8 @@ pipeline {
                     awsEcsDeployApp([
                         awsRegion: "${awsRegion}",
                         awsCredId: "aws-${DEPARTMENT}-admin",
+                        ecsClusterRegex="^.*/CL.*-${APP_NAME}\$",
+                        ecsServiceRegex="^.*/SVC-${awsAppName}",
                         awsEcrImg: "${awsEcrImg}",
                         awsAppEnv: "${awsAppEnv}",
                         awsAppName: "${APP_NAME}",
