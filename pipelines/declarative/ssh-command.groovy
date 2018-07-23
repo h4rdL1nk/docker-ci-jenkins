e580c140cb58193fca85f89c7c3b927bc52c328f
h4rdL1nk <imthedanger@protonmail.com>
2018-07-23 15:04:08 +0200

    Added notifications

--- pipelines/declarative/ssh-command.groovy
+++ pipelines/declarative/ssh-command.groovy
@@ -1,8 +1,16 @@
+@Library('standardLibraries') _
+
 node {
+
     def remote = [:]
     
     withCredentials([sshUserPrivateKey(credentialsId: 'ssh-privkey-sdops', keyFileVariable: 'privkey', usernameVariable: 'userName')]) {
     
+        stage('Init GIT'){
+            sh script: 'git init'
+            notify([ type: "slack-default-start" ])
+        }
+
         stage('Configure credentials object'){
             remote.user = userName
             remote.identityFile = privkey
@@ -10,15 +18,18 @@ node {
             remote.host = 'jenkins.smartdigits.io'
             remote.allowAnyHosts = true
         }
+
         stage('Generate backup file over SSH'){
             sshCommand remote: remote, command: '''
     		    sudo tar -c --exclude="workspace/*" /var/lib/jenkins/ | gzip -9 >/tmp/jenkins.tar.gz
     	    '''
         }
+
         stage('Get backup file over SCP'){
             sshGet remote: remote, from: '/tmp/jenkins.tar.gz', into: 'jenkins.tar.gz', override: true
             sshRemove remote: remote, path: '/tmp/jenkins.tar.gz'
         }
+
         stage('Upload backup file over SCP'){
             remote.name = 'opsmgr.smartdigits.io'
             remote.host = 'opsmgr.smartdigits.io' 
@@ -30,8 +41,14 @@ node {
                 BKFILE_NUM=5
                 
                 sudo mv /tmp/jenkins.tar.gz \${BKDIR}jenkins-\$(date "+%Y%m%d").tar.gz
-                ls -rt \$BKDIR | head -\$(( \$(ls \$BKDIR | wc -w) - \$BKFILE_NUM )) | while read F;do echo "Removing \$BKDIR/\$F ...";rm \$BKDIR/\$F;done
+                sudo ls -rt \$BKDIR | head -\$(( \$(sudo ls \$BKDIR | wc -w) - \$BKFILE_NUM )) | while read F;do echo "Removing \$BKDIR/\$F ...";sudo rm \$BKDIR/\$F;done
             '''
         }
+
+        post{
+            notify([ type: "slack-default-end" ])
+        }
+
     }
+
 }
