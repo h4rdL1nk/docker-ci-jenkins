a5009c0ae0b4eb3b93f261f6dbb059e7a7fc17e2
h4rdL1nk <imthedanger@protonmail.com>
2018-07-23 17:34:50 +0200

    Final version for pipeline version

--- pipelines/declarative/ssh-command.groovy
+++ pipelines/declarative/ssh-command.groovy
@@ -1,53 +1,75 @@
 @Library('standardLibraries') _
 
-node {
+def remote = [:]
 
-    def remote = [:]
+pipeline {
+
+    agent any
     
-    withCredentials([sshUserPrivateKey(credentialsId: 'ssh-privkey-sdops', keyFileVariable: 'privkey', usernameVariable: 'userName')]) {
-    
-        stage('Notification'){
-            notify([ type: "slack-default-start" ])
+    stages{
+
+        stage('Send notification'){
+            steps{
+                script{
+                    notify([ type: "slack-default-start" ])
+                }
+            }
         }
 
-        stage('Configure credentials object'){
-            remote.user = userName
-            remote.identityFile = privkey
-    	    remote.name = 'jenkins.smartdigits.io'
-            remote.host = 'jenkins.smartdigits.io'
-            remote.allowAnyHosts = true
-        }
-
-        stage('Generate backup file over SSH'){
-            sshCommand remote: remote, command: '''
-    		    sudo tar -c --exclude="workspace/*" /var/lib/jenkins/ | gzip -9 >/tmp/jenkins.tar.gz
-    	    '''
-        }
-
-        stage('Get backup file over SCP'){
-            sshGet remote: remote, from: '/tmp/jenkins.tar.gz', into: 'jenkins.tar.gz', override: true
-            sshRemove remote: remote, path: '/tmp/jenkins.tar.gz'
-        }
-
-        stage('Upload backup file over SCP'){
-            remote.name = 'opsmgr.smartdigits.io'
-            remote.host = 'opsmgr.smartdigits.io' 
-            sshPut remote: remote, from: 'jenkins.tar.gz', into: '/tmp/jenkins.tar.gz'
-            sshCommand remote: remote, command: '''
-                #!/bin/bash
+        stage('Backup process'){
                 
-                BKDIR="/var/dockershared/storage/ds1_oplog/jenkins/"
-                BKFILE_NUM=5
-                
-                sudo mv /tmp/jenkins.tar.gz \${BKDIR}jenkins-\$(date "+%Y%m%d").tar.gz
-                sudo ls -rt \$BKDIR | head -\$(( \$(sudo ls \$BKDIR | wc -w) - \$BKFILE_NUM )) | while read F;do echo "Removing \$BKDIR/\$F ...";sudo rm \$BKDIR/\$F;done
-            '''
+            steps{
+
+                withCredentials([sshUserPrivateKey(credentialsId: 'ssh-privkey-sdops', keyFileVariable: 'privkey', usernameVariable: 'userName')]) {
+
+                    script{
+                        
+                        script{
+                            remote = [
+                                user: userName,
+                                identityFile: privKey,
+                                name: 'jenkins.smartdigits.io',
+                                host: 'jenkins.smartdigits.io',
+                                allowAnyHosts: true
+                            ]
+                        }
+
+                        sshCommand remote: remote, command: '''
+                            sudo tar -c --exclude="workspace/*" /var/lib/jenkins/ | gzip -9 >/tmp/jenkins.tar.gz
+                        '''
+
+                        sshGet remote: remote, from: '/tmp/jenkins.tar.gz', into: 'jenkins.tar.gz', override: true
+                        
+                        sshRemove remote: remote, path: '/tmp/jenkins.tar.gz'                            
+                    
+                        sshPut remote: remote, from: 'jenkins.tar.gz', into: '/tmp/jenkins.tar.gz'
+                    
+                        sshCommand remote: remote, command: '''
+                            #!/bin/bash
+                        
+                            BKDIR="/var/dockershared/storage/ds1_oplog/jenkins/"
+                            BKFILE_NUM=5
+                        
+                            sudo mv /tmp/jenkins.tar.gz \${BKDIR}jenkins-\$(date "+%Y%m%d").tar.gz
+                            sudo ls -rt \$BKDIR | head -\$(( \$(sudo ls \$BKDIR | wc -w) - \$BKFILE_NUM )) | while read F;do echo "Removing \$BKDIR/\$F ...";sudo rm \$BKDIR/\$F;done
+                        '''
+                    }
+
+                }
+
+            }
+
         }
 
-        post{
-            notify([ type: "slack-default-end" ])
-        }
+    }
 
+    post{
+        always{
+            deleteDir()
+            script{
+                notify([ type: "slack-default-end" ])
+            }
+        }
     }
 
 }
---
+++ pipelines/script/ssh-command.groovy
@@ -0,0 +1,37 @@
+node {
+    def remote = [:]
+    
+    withCredentials([sshUserPrivateKey(credentialsId: 'ssh-privkey-sdops', keyFileVariable: 'privkey', usernameVariable: 'userName')]) {
+    
+        stage('Configure credentials object'){
+            remote.user = userName
+            remote.identityFile = privkey
+    	    remote.name = 'jenkins.smartdigits.io'
+            remote.host = 'jenkins.smartdigits.io'
+            remote.allowAnyHosts = true
+        }
+        stage('Generate backup file over SSH'){
+            sshCommand remote: remote, command: '''
+    		    sudo tar -c --exclude="workspace/*" /var/lib/jenkins/ | gzip -9 >/tmp/jenkins.tar.gz
+    	    '''
+        }
+        stage('Get backup file over SCP'){
+            sshGet remote: remote, from: '/tmp/jenkins.tar.gz', into: 'jenkins.tar.gz', override: true
+            sshRemove remote: remote, path: '/tmp/jenkins.tar.gz'
+        }
+        stage('Upload backup file over SCP'){
+            remote.name = 'opsmgr.smartdigits.io'
+            remote.host = 'opsmgr.smartdigits.io' 
+            sshPut remote: remote, from: 'jenkins.tar.gz', into: '/tmp/jenkins.tar.gz'
+            sshCommand remote: remote, command: '''
+                #!/bin/bash
+                
+                BKDIR="/var/dockershared/storage/ds1_oplog/jenkins/"
+                BKFILE_NUM=5
+                
+                sudo mv /tmp/jenkins.tar.gz \${BKDIR}jenkins-\$(date "+%Y%m%d").tar.gz
+                sudo ls -rt \$BKDIR | head -\$(( \$(sudo ls \$BKDIR | wc -w) - \$BKFILE_NUM )) | while read F;do echo "Removing \$BKDIR/\$F ...";sudo rm \$BKDIR/\$F;done
+            '''
+        }
+    }
+}
