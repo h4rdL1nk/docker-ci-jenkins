d689f0b9528e4fa1bc76c0df9bb33d9b23b91cc1
h4rdL1nk <imthedanger@protonmail.com>
2018-07-23 14:08:56 +0200

    Updated

--- pipelines/declarative/ssh-command.groovy
+++ pipelines/declarative/ssh-command.groovy
@@ -1,15 +1,37 @@
-
 node {
-  stage('Remote SSH') {
+    def remote = [:]
+    
     withCredentials([sshUserPrivateKey(credentialsId: 'ssh-privkey-sdops', keyFileVariable: 'privkey', usernameVariable: 'userName')]) {
-	def remote = [:]
-        remote.user = userName
-        remote.identityFile = privkey
-	remote.name = '192.168.247.101'
-        remote.host = '192.168.247.101'
-        remote.allowAnyHosts = true
-        sshCommand remote: remote, command: 'uptime'
+    
+        stage('Configure credentials object'){
+            remote.user = userName
+            remote.identityFile = privkey
+    	    remote.name = 'jenkins.smartdigits.io'
+            remote.host = 'jenkins.smartdigits.io'
+            remote.allowAnyHosts = true
+        }
+        stage('Generate backup file over SSH'){
+            sshCommand remote: remote, command: '''
+    		    sudo tar -c --exclude="workspace/*" /var/lib/jenkins/ | gzip -9 >/tmp/jenkins.tar.gz
+    	    '''
+        }
+        stage('Get backup file over SCP'){
+            sshGet remote: remote, from: '/tmp/jenkins.tar.gz', into: 'jenkins.tar.gz', override: true
+            sshRemove remote: remote, path: '/tmp/jenkins.tar.gz'
+        }
+        stage('Upload backup file over SCP'){
+            remote.name = 'opsmgr.smartdigits.io'
+            remote.host = 'opsmgr.smartdigits.io' 
+            sshPut remote: remote, from: 'jenkins.tar.gz', into: '/tmp/jenkins.tar.gz'
+            sshCommand remote: remote, command: '''
+                #!/bin/bash
+                
+                BKDIR="/var/dockershared/storage/ds1_oplog/jenkins/"
+                BKFILE_NUM=5
+                
+                sudo mv /tmp/jenkins.tar.gz \${BKDIR}jenkins-\$(date "+%Y%m%d").tar.gz
+                ls -rt \$BKDIR | head -\$(( \$(ls \$BKDIR | wc -w) - \$BKFILE_NUM )) | while read F;do echo "Removing \$BKDIR/\$F ...";rm \$BKDIR/\$F;done
+            '''
+        }
     }
-  }
 }
-
